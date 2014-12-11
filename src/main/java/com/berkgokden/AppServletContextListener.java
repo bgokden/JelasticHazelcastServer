1e13fc81a8e0cdec9fd9beb365756eaa52097040
Berk Gokden <berkgokden@gmail.com>
2014-12-11 22:04:01 +0200

    build worked :)

---
+++ src/main/java/com/berkgokden/AppServletContextListener.java
@@ -0,0 +1,17 @@
+package com.berkgokden;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class AppServletContextListener implements ServletContextListener{
+    @Override
+    public void contextDestroyed(ServletContextEvent arg0) {
+        System.out.println("ServletContextListener destroyed");
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent arg0) {
+        System.out.println("ServletContextListener started");
+        new HazelcastServer().run();
+    }
+}
---
+++ src/main/java/com/berkgokden/HazelcastServer.java
@@ -0,0 +1,29 @@
+package com.berkgokden;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.Map;
+import java.util.Queue;
+
+public class HazelcastServer {
+
+    public void run() {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<Integer, String> mapCustomers = hazelcastInstance.getMap("customers");
+        mapCustomers.put(1, "Joe");
+        mapCustomers.put(2, "Ali");
+        mapCustomers.put(3, "Avi");
+
+        System.out.println("Customer with key 1: " + mapCustomers.get(1));
+        System.out.println("Map Size:" + mapCustomers.size());
+
+        Queue queueCustomers = hazelcastInstance.getQueue("customers");
+        queueCustomers.offer("Tom");
+        queueCustomers.offer("Mary");
+        queueCustomers.offer("Jane");
+        System.out.println("First customer: " + queueCustomers.poll());
+        System.out.println("Second customer: " + queueCustomers.peek());
+        System.out.println("Queue size: " + queueCustomers.size());
+    }
+}
