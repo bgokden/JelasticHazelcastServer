dd2012a284cebf1b6fdd9d41aedd79eceb19aa25
Berk Gokden <berkgokden@gmail.com>
2014-12-29 11:56:56 +0200

    mongo db back up

--- src/main/java/com/berkgokden/AppServletContextListener.java
+++ src/main/java/com/berkgokden/AppServletContextListener.java
@@ -1,7 +1,11 @@
 package com.berkgokden;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.Map;
 
 public class AppServletContextListener implements ServletContextListener{
     @Override
@@ -12,6 +16,12 @@ public class AppServletContextListener implements ServletContextListener{
     @Override
     public void contextInitialized(ServletContextEvent arg0) {
         System.out.println("ServletContextListener started");
-        new HazelcastServer().run();
+        //new HazelcastServer().run();
+        ApplicationContext ctx = new GenericXmlApplicationContext("beans.xml");
+        Map map= (Map)ctx.getBean("usermap");
+        User usr = new User("Enes", 29);
+        map.put("id-134", usr);
+        User usr2 = (User) map.get("id-134");
+        System.out.println(usr2.getName());
     }
 }
