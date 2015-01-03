a34e86d294127ae88d333f0c052cf3de2e810116
Berk Gokden <berkgokden@gmail.com>
2015-01-03 17:53:52 +0200

    json trials

---
+++ src/main/java/com/berkgokden/model/AnyJSON.java
@@ -0,0 +1,10 @@
+package com.berkgokden.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+/**
+ * Created by berkgokden on 12/30/14.
+ */
+public class AnyJSON extends HashMap<String, Serializable> {
+}
--- src/main/java/com/berkgokden/rest/JSONService.java
+++ src/main/java/com/berkgokden/rest/JSONService.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Map;
 
 @Path("/json")
 public class JSONService {
@@ -40,6 +41,37 @@ public class JSONService {
 
         String result = "Track saved : " + track;
         return Response.status(201).entity(result).build();
+
+    }
+
+    @POST
+    @Path("/insert")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createAnythingInJSON(Map<String, Object> anything) {
+
+        String result = "Anything saved : " + anything;
+        return Response.status(201).entity(result).build();
+
+    }
+
+    @GET
+    @Path("/receive")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getAnythingkInJSON() {
+
+        Map<String, Object> anything = new HashMap<String, Object>();
+
+        anything.put("el1", 5);
+        anything.put("el2", 5.3);
+        anything.put("el3", "String test element");
+        Map<String, Object> anything2 = new HashMap<String, Object>();
+        anything2.put("2el1", 6);
+        anything2.put("2el2", 6.4);
+        anything2.put("2el3", "String test element 2");
+        anything.put("el4", anything2);
+
+        return anything;
 
     }
 
